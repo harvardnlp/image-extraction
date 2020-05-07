3f3b7098059a78a4e3bf5e951f488074c75c8109
Alex Rush <sasha.rush@gmail.com>
2020-05-07 09:25:36 -0400

    Base code

--- run.py
+++ run.py
@@ -3,32 +3,55 @@ from PIL import Image
 import IPython
 import glob
 from math import log
+import argparse
 
 os.system("mkdir dataset")
 os.system("mkdir out")
 
-for x in undone:
-    for g_f in glob.glob("papers/"+ x + ".pdf"):
+def main(args):
+    for g_f in glob.glob(args.data_dir + "/*.pdf"):
         g = g_f.split("/")[-1]
         os.system("rm dataset/*")
         os.system("rm out/*")
-        command = "convert -background white  -alpha remove -alpha off -density 200 '" + g_f + "'[1-12]  png24:dataset/" +g +"-%04d.png"
+        command = (
+            "convert -background white  -alpha remove -alpha off -density 200 '"
+            + g_f
+            + "'[1-12]  png24:dataset/"
+            + g
+            + "-%04d.png"
+        )
         print(command)
         os.system(command)
-        command2 = "python infer_simple.py  --cfg e2e_faster_rcnn_X-101-64x4d-FPN_1x.yaml --output-dir out --image-ext png --wts model_final.pkl dataset/ > tmp 2> tmp.err"
+        command2 = (
+            "python infer_simple.py "
+            + " --cfg e2e_faster_rcnn_X-101-64x4d-FPN_1x.yaml "
+            + " --output-dir out "
+            + " --image-ext png "
+            + " --wts model_final.pkl dataset/ > tmp 2> tmp.err"
+        )
         os.system(command2)
         print(g)
         if not os.path.exists("out/out.csv"):
             print("failed")
-            break
             continue
         d = -1e9
         for i, l in enumerate(open("out/out.csv")):
             print(i)
-            f,  x0, y0, x1, y1, _ = l.strip().split(";")
+            f, x0, y0, x1, y1, _ = l.strip().split(";")
             print(f)
-            original = Image.open("dataset/"+ f)
-            cropped_example = original.crop((int(x0)-20, int(y0)-20, int(x1)+20, int(y1)+20))
-            cropped_example.save("pics/" + f + "." + str(i) + ".png")
-    break
+            original = Image.open("dataset/" + f)
+            cropped_example = original.crop(
+                (int(x0) - 20, int(y0) - 20, int(x1) + 20, int(y1) + 20)
+            )
+            cropped_example.save(out_dir + "/" + f + "." + str(i) + ".png")
 
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument("data_dir", help="Dataset path with pdfs")
+    parser.add_argument("out_dir", help="Output path with pngs")
+    args = parser.parse_args()
+    main(args)
